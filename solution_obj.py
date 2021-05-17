9c2b8fa2ef50e93f8ddcb88225136a228ccd64fe
Jelle <jelle.verstappen@telenet.be>
2021-05-17 10:03:20 +0200

    Update 10h

--- solution_obj.py
+++ solution_obj.py
@@ -7,17 +7,20 @@ class Game:
         """Zet de stand op de beginstand bij aanvang van de wedstrijd"""
         # TODO: Gebruik onderstaande member variables,
         # maar corrigeer de initiele waardes:
-        self.points_home = 99
-        self.points_away = 99
-        self.sets_home = 99
-        self.sets_away = 99
-        self.set_nr = 99
+        self.points_home = 0
+        self.points_away = 0
+        self.sets_home = 0
+        self.sets_away = 0
+        self.set_nr = 1
 
     def get_max_points(self):
         """Geef het maximaal aantal punten voor deze set terug."""
         # TODO: Geef de correcte waarde terug afhankelijk van
         # de set_nr.
-        return 99
+        maxpoints = 25
+        if self.set_nr == 5:
+            maxpoints = 15
+        return maxpoints
 
     def score(self, team):
         """Bereken de nieuwe stand als team `team` gescoord heeft.
@@ -26,3 +29,20 @@ class Game:
         # TODO: Pas hier de punten, set punten en set nummer aan.
         # Gebruik `get_max_points` om te weten bij hoeveel punten je
         # in de set de set gewonnen hebt.
+        if team == "A":
+            self.points_away = self.points_away + 1
+
+        if team == "H":
+            self.points_home = self.points_home + 1
+
+        if self.points_home >= Game.get_max_points() and self.points_home >= self.points_away + 2:
+            self.points_home = 0
+            self.points_away = 0
+            self.sets_home = self.sets_home + 1
+            self.set_nr = self.set_nr + 1
+        
+        if self.points_away >= Game.get_max_points() and self.points_away >= self.points_home + 2:
+            self.points_home = 0
+            self.points_away = 0
+            self.sets_away = self.sets_away + 1
+            self.set_nr = self.set_nr + 1
